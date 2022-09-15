ebc365e9348f99d15895b4fc13006783710d869a
Gledson A <g_a6@live.com>
2022-09-14 21:35:38 -0300

    Implementing way to return the html content generated by the render class

--- src/main/java/cotuba/RenderizadorMDParaHTML.java
+++ src/main/java/cotuba/RenderizadorMDParaHTML.java
@@ -5,6 +5,8 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.commonmark.node.AbstractVisitor;
@@ -15,13 +17,16 @@ import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 public class RenderizadorMDParaHTML {
-  public void renderiza(Path diretorioDosMD) {
+  public List<Capitulo> renderiza(Path diretorioDosMD) {
+    List<Capitulo> capitulos = new ArrayList<>();
+
     PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
     try (Stream<Path> arquivosMD = Files.list(diretorioDosMD)) {
       arquivosMD
           .filter(matcher::matches)
           .sorted()
           .forEach(arquivoMD -> {
+            Capitulo capitulo = new Capitulo();
             Parser parser = Parser.builder().build();
             Node document = null;
             try {
@@ -32,7 +37,7 @@ public class RenderizadorMDParaHTML {
                   if (heading.getLevel() == 1) {
                     // capítulo
                     String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
-                    // TODO: usar título do capítulo
+                    capitulo.setTitulo(tituloDoCapitulo);
                   } else if (heading.getLevel() == 2) {
                     // seção
                   } else if (heading.getLevel() == 3) {
@@ -49,8 +54,8 @@ public class RenderizadorMDParaHTML {
               HtmlRenderer renderer = HtmlRenderer.builder().build();
               String html = renderer.render(document);
 
-              // PDF ou EPUB aqui???
-
+              capitulo.setConteudoHTML(html);
+              capitulos.add(capitulo);
             } catch (Exception ex) {
               throw new IllegalStateException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
             }
@@ -58,5 +63,7 @@ public class RenderizadorMDParaHTML {
     } catch (IOException ex) {
       throw new IllegalStateException("Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
     }
+
+    return capitulos;
   }
 }
